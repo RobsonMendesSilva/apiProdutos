60dbbc6995143121742b4842b780747fb33f33da
RobsonMendesSilva <menxweb@gmail.com>
2026-04-06 17:54:52 -0300

    Estrutura inicial da API de Produtos com Spring Boot

---
+++ src/test/java/br/com/robson/ApiProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.robson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
